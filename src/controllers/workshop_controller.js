a75c512f4b0fb4e5acb28d92a569f6660d69a7eb
Gabriel Pereira Neves <157249929+nevespgabriel@users.noreply.github.com>
2025-02-05 00:44:31 -0300

    Update workshop_controller.js

--- src/controllers/workshop_controller.js
+++ src/controllers/workshop_controller.js
@@ -9,25 +9,25 @@ const store = async(req, res) => {
         });
         res.status(201).json(novo);
     } catch(error){
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 }
 
 const index = async(req, res) => {
     try{
-        const items = await Workshop.find();
-        res.status(200).json(items);
+        const items = await Workshop.find().exec();
+        res.json(items);
     } catch(error){
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 }
 
 const show = async(req, res) => {
     try{
-        const item = await Workshop.findById(req.params.id).populate("maintenances");
-        res.status(200).json(item);
+        const item = await Workshop.findById(req.params.id).populate("maintenances").exec();
+        res.json(item);
     } catch(error){
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 }
 
@@ -38,19 +38,19 @@ const update = async(req, res) => {
             address: req.body.address,
             specialties: req.body.specialties
         }
-        await Workshop.findByIdAndUpdate(req.params.id, novo);
+        await Workshop.findByIdAndUpdate(req.params.id, novo).exec();
         res.status(200).json(novo);
     } catch(error){
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 }
 
 const destroy = async(req, res) => {
     try{
-        await Workshop.findByIdAndDelete(req.params.id);
+        await Workshop.findByIdAndDelete(req.params.id).exec();
         res.status(200);
     } catch(error){
-        res.status(400).json(error);
+        res.status(500).json(error);
     }
 }
 
