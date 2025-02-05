757b9c64a50f2f1cff61304a15578bae16990eee
Gabriel Pereira Neves <157249929+nevespgabriel@users.noreply.github.com>
2025-02-05 00:34:04 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-import dotenv from "dotenv";
+import "dotenv/config"
+import "./config/conn.js"
 import e from "express";
 import maintenance_route from "./routes/maintenance_route";
 import vehicle_route from "./routes/vehicle_route.js";
 import workshop_route from "./routes/workshop_route.js";
 
 const app = e();
-app.use(e.json);
+app.use(e.json());
 
 app.use("/maintenance", maintenance_route);
 app.use("/vehicle", vehicle_route);
 app.use("/workshop", workshop_route);
 
-app.listen(() => {
+app.listen(process.env.API_PORT, () => {
     console.log(`Server running at ${process.env.API_PORT}`);
-}, process.env.API_PORT);
+});
